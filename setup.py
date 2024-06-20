3fcadf39b9078c8d4a01de31fcabe68e71f24de2
jayraj2498 <jayraj2498@gmail.com>
2024-06-20 16:33:20 +0530

    saved changes

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup 
+#from typing import List
+
+
+
+
+setup(
+    name="us_visa_approval",
+    version="0.0.0",
+    author="jayraj",
+    author_email="jayraj6699@gmail.com.com",
+    packages=find_packages()
+)  
+
+
